7bde540e9db5976a7622f5e376970b3a7142dfb8
rajeshgp <rajeshgnanam207@gmail.com>
2018-09-25 15:14:09 +0530

    OLE-9345 - Courtesty Notice FreeMarker template error: itemInfo.item

--- ole-app/olefs/src/main/java/org/kuali/ole/deliver/service/LostNoticesExecutor.java
+++ ole-app/olefs/src/main/java/org/kuali/ole/deliver/service/LostNoticesExecutor.java
@@ -52,6 +52,11 @@ public class LostNoticesExecutor extends LoanNoticesExecutor {
     }
 
     @Override
+    public void saveLoanDocument() {
+        getBusinessObjectService().save(loanDocuments);
+    }
+
+    @Override
     protected String getNoticeType() {
         return OLEConstants.NOTICE_LOST;
     }
